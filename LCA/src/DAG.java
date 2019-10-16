2200b1c577fd8f21852753443c558e566a2de4cc
Odhran <mullano@tcd.ie>
2019-10-16 15:32:51 +0100

    added LCA and started testing

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class DAG {
 	private final int V;
@@ -46,14 +48,84 @@ public class DAG {
 		return dfsObj.visited(y);
 	}
 	
+	public ArrayList<Integer> lowestCommonAncestor(int x, int y)
+	{	
+		ArrayList<Integer> LCA = new ArrayList<Integer>();
+		int currentMax = Integer.MAX_VALUE;
+		
+		if (x==y) { 
+			return LCA;
+		} else if (x >= this.V) {
+			return LCA;
+		} else if (y >= this.V) {
+			return LCA; 
+		} else if (x < 0) {
+			return LCA;
+		} else if ( y < 0) {
+			return LCA;
+		}
+		
+		DirectedDFS depthFirstSearch = new DirectedDFS(this, x);
+		depthFirstSearch.reverseDfs(this, x);
+		int distanceX, distanceY;
+		
+		for(int v = 0; v < this.V; v++) {
+			if(depthFirstSearch.reverseVisited(v) && hasPath(v, y)) {
+				distanceX = getDistance(v, x);
+				distanceY = getDistance(v, y);
+				
+				if(Integer.max(distanceX, distanceY) < currentMax) {		
+					LCA = new ArrayList<Integer>();
+					LCA.add(v);
+					currentMax = Integer.max(distanceX, distanceY);
+				} else if(Integer.max(distanceX, distanceY) == currentMax) {
+					LCA.add(v);
+					currentMax = Integer.max(distanceX, distanceY);
+				}
+			}
+		}
+		return LCA;
+	}
+
+	private int getDistance(int x, int target) {
+	    
+			if(x == target) { 
+				return 0; 
+			} else {
+		        Queue<Integer> queue = new LinkedList<Integer>();
+		        int[] distance = new int[this.V];
+		        boolean[] marked = new boolean[this.V];
+		        
+		        for (int v = 0; v < this.V(); v++){   
+		        	distance[v] = Integer.MAX_VALUE;
+		        }
+		        
+		        distance[x] = 0;
+		        marked[x] = true;
+		        queue.add(x);
+		        
+		        while (!queue.isEmpty()) {
+		            int v = queue.remove();
+		            for (int w : this.adj(v)) {
+		                if (!marked[w]) {
+		                	distance[w] = distance[v] + 1;
+		                    marked[w] = true;
+		                    queue.add(w);
+		                }
+		            }
+		        }
+		        return distance[target];
+			}
+	}
+	
 	
 	private class DirectedDFS {
 		private boolean[] marked;
-		private boolean[] revMarked;
+		private boolean[] reverseMarked;
 		
 		public DirectedDFS(DAG G, int s) {
 			marked = new boolean[G.V()];
-			revMarked = new boolean[G.V()];
+			reverseMarked = new boolean[G.V()];
 			dfs(G, s);
 		}
 		
@@ -64,17 +136,32 @@ public class DAG {
 		}
 		
 		private void reverseDfs(DAG G, int v){
-			revMarked[v] = true;
+			reverseMarked[v] = true;
 			for (int w : G.reverseAdj(v))
-			if (!revMarked[w]) reverseDfs(G, w);
+			if (!reverseMarked[w]) {
+				reverseDfs(G, w);
+			}
 		}
 		
 		public boolean visited(int v) { 
 			return marked[v]; 
 		}
 		
-		public boolean revVisited(int v){ 
-			return revMarked[v]; 
+		public boolean reverseVisited(int v){ 
+			return reverseMarked[v]; 
+		}
+	}
+	
+	public static void main(String args[]) {
+		DAG dag = new DAG(5);
+		boolean x = dag.addEdge(1, 2);
+		x = dag.addEdge(1, 4);
+		x = dag.addEdge(2, 3);
+		x = dag.addEdge(4, 5);
+		ArrayList<Integer> result = dag.lowestCommonAncestor(3,5);
+		System.out.print(result.size());
+		for(int i = 0; i < result.size(); i++) {
+			System.out.print(result.get(i));
 		}
 	}
 }
---
+++ LCA/src/DAGTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class DAGTest {
+
+	@Test
+	void test() {
+		/*DAG dag = new DAG(5);
+		boolean x = dag.addEdge(1, 2);
+		x = dag.addEdge(1, 4);
+		x = dag.addEdge(2, 3);
+		x = dag.addEdge(4, 5);
+		ArrayList<Integer> result = dag.lowestCommonAncestor(1,5);
+		for(int i = 0; i < result.size(); i++) {
+			System.out.print(result.get(i));
+		}
+		*/
+	}
+
+}
